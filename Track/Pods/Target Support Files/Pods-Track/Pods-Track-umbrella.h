d65142297875fb70bb7da0c528a3b8999ac8115a
Melissa <melbrng@gmail.com>
2016-07-16 13:32:03 -0700

    added Track xcode project with Firebase podfile

---
+++ Track/Pods/Target Support Files/Pods-Track/Pods-Track-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_TrackVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_TrackVersionString[];
+
---
+++ Track/Track/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Track
+//
+//  Created by Melissa Boring on 7/16/16.
+//  Copyright © 2016 melbo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
