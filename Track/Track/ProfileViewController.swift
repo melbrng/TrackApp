7bf98bb3569dfa9196d5d80819ea5b3777425660
Melissa <melbrng@gmail.com>
2016-08-30 10:51:50 -0700

    reworked ProfileVC , implemented Extensions for protocol conformance and readability

--- Track/Track/ProfileViewController.swift
+++ Track/Track/ProfileViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 import QuartzCore
 
-class ProfileViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate,UICollectionViewDelegateFlowLayout {
+class ProfileViewController: UIViewController{
     
     var trackArray = firebaseHelper.trackArray
     var trackFootprints = [Footprint]()
@@ -21,11 +21,7 @@ class ProfileViewController: UIViewController, UICollectionViewDataSource, UICol
     override func viewDidLoad() {
         
         //remove "Add New Track" track
-        //trackArray.removeAtIndex(0)
-        trackArray.removeRange(0...1)
-        
-
-
+        trackArray.removeAtIndex(0)
         let rightBarButtonImage : UIImage? = UIImage(named:"ic_mode_edit.png")!.imageWithRenderingMode(.AlwaysOriginal)
         
         let leftBarButtonImage : UIImage? = UIImage(named:"ic_arrow_back.png")!.imageWithRenderingMode(.AlwaysOriginal)
@@ -47,82 +43,18 @@ class ProfileViewController: UIViewController, UICollectionViewDataSource, UICol
 
     }
     
+    //MARK: UI
     @IBAction func editTracks(sender: AnyObject) {
-        
         trackCollectionView.allowsMultipleSelection = true
-        
-    }
-    
-    func collectionView(collectionView: UICollectionView, didHighlightItemAtIndexPath indexPath: NSIndexPath) {
-        print("highlight")
-        
-        let cell = collectionView.cellForItemAtIndexPath(indexPath) as! ProfileCollectionViewCell
-        
-        UIView.animateWithDuration(0.1,
-                                   delay: 0,
-                                   options: .AllowUserInteraction,
-                                   animations: {
-                                   cell.backgroundColor = UIColor.redColor().colorWithAlphaComponent(0.5)},
-                                   completion: nil)
     }
     
     
-    //MARK: Collection View Delegate
-    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return trackArray.count
+    @IBAction func cancelProfile(sender: AnyObject) {
+        self.navigationController?.popViewControllerAnimated(true)
     }
     
-   
-    func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAtIndexPath indexPath: NSIndexPath) -> CGSize
-    {
-
-        
-        let totalwidth = collectionView.bounds.size.width;
-        let numberOfCellsPerRow = 3
-        let dimensions = CGFloat(Int(totalwidth) / numberOfCellsPerRow)
-        return CGSizeMake(dimensions, dimensions)
-
-
-    }
-    
-    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
-        
-        let cellIdentifier = "TrackCell"
-        
-        let cell = collectionView .dequeueReusableCellWithReuseIdentifier(cellIdentifier, forIndexPath: indexPath) as! ProfileCollectionViewCell
-        
-        cell.trackLabel.backgroundColor = UIColor.blackColor().colorWithAlphaComponent(0.5)
-        cell.trackLabel.textColor = UIColor.whiteColor()
-        cell.trackLabel.text = trackArray[indexPath.item].trackName
- 
-        cell.trackImageView.image = trackArray[indexPath.item].trackImage
-        cell.layer.borderWidth = 0.5
-        cell.layer.borderColor = UIColor.blackColor() .CGColor
-        cell.backgroundColor = UIColor .whiteColor()
-        
-        return cell
-    }
-    
-    func collectionView(collectionView: UICollectionView, didSelectItemAtIndexPath indexPath: NSIndexPath) {
-        
-        print("selected " + String(indexPath.item))
-        
-        let cell = collectionView.cellForItemAtIndexPath(indexPath) as! ProfileCollectionViewCell
-        cell.isSelected(true)
-        
-        
-        //collectionView.deleteItemsAtIndexPaths([indexPath])
-        
-        //trackArray.removeAtIndex(indexPath.item)
-    }
-    
-
-    
-
-
-    
+    //MARK: Segue
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        
         
         if(segue.identifier == "ShowFootprints"){
             
@@ -139,12 +71,69 @@ class ProfileViewController: UIViewController, UICollectionViewDataSource, UICol
             destinationTableViewController.trackName = trackArray[indexPath.item].trackName
             destinationTableViewController.footprintsArray = trackFootprints
             
+            }
         }
+    
     }
 
-    
-    @IBAction func cancelProfile(sender: AnyObject) {
-         self.navigationController?.popViewControllerAnimated(true)
+    //MARK: Collection View Delegate
+    extension ProfileViewController:UICollectionViewDataSource, UICollectionViewDelegate,UICollectionViewDelegateFlowLayout{
+        func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+            return trackArray.count
+        }
+        
+       
+        func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAtIndexPath indexPath: NSIndexPath) -> CGSize
+        {
+
+            
+            let totalwidth = collectionView.bounds.size.width;
+            let numberOfCellsPerRow = 3
+            let dimensions = CGFloat(Int(totalwidth) / numberOfCellsPerRow)
+            return CGSizeMake(dimensions, dimensions)
+
+
+        }
+        
+        func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+            
+            let cellIdentifier = "TrackCell"
+            
+            let cell = collectionView .dequeueReusableCellWithReuseIdentifier(cellIdentifier, forIndexPath: indexPath) as! ProfileCollectionViewCell
+            
+            cell.trackLabel.backgroundColor = UIColor.blackColor().colorWithAlphaComponent(0.5)
+            cell.trackLabel.textColor = UIColor.whiteColor()
+            cell.trackLabel.text = trackArray[indexPath.item].trackName
+     
+            cell.trackImageView.image = trackArray[indexPath.item].trackImage
+            cell.layer.borderWidth = 0.5
+            cell.layer.borderColor = UIColor.blackColor() .CGColor
+            cell.backgroundColor = UIColor .whiteColor()
+            
+            return cell
+        }
+        
+        func collectionView(collectionView: UICollectionView, didSelectItemAtIndexPath indexPath: NSIndexPath) {
+
+            let cell = collectionView.cellForItemAtIndexPath(indexPath) as! ProfileCollectionViewCell
+            cell.isSelected(true)
+            
+            //collectionView.deleteItemsAtIndexPaths([indexPath])
+            
+            //trackArray.removeAtIndex(indexPath.item)
+        }
+        
+
+        func collectionView(collectionView: UICollectionView, didHighlightItemAtIndexPath indexPath: NSIndexPath) {
+            
+            let cell = collectionView.cellForItemAtIndexPath(indexPath) as! ProfileCollectionViewCell
+            
+            UIView.animateWithDuration(0.1,
+                                       delay: 0,
+                                       options: .AllowUserInteraction,
+                                       animations: {
+                                        cell.backgroundColor = UIColor.redColor().colorWithAlphaComponent(0.5)},
+                                       completion: nil)
+        }
+       
     }
-   
-}
